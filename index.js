250c721d99437b0375f58d59024b8043690b157a
agathaturquete <00001093300267sp@al.educacao.sp.gov.br>
2024-11-26 20:21:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+const filmes = [[
+    "Exterminador do futuro",
+    "https://images6.alphacoders.com/614/614635.jpg",
+    "https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/92/90/99/20224665.jpg",
+    "Num futuro próximo, a guerra entre humanos e máquinas foi deflagrada. Com a tecnologia a seu dispor, um plano inusitado é arquitetado pelas máquinas ao enviar para o passado um andróide (Arnold Schwarzenegger) com a missão de matar a mãe (Linda Hamilton) daquele que viria a se transformar num líder e seu pior inimigo. Contudo, os humanos também conseguem enviar um representante (Michael Biehn) para proteger a mulher e tentar garantir o futuro da humanidade."
+],
+[
+    "Gladiador 2",
+    "https://th.bing.com/th/id/OIP.uoKwWHjTX3vhIEg2lz9R4QHaEK?rs=1&pid=ImgDetMain",
+    "https://br.web.img3.acsta.net/c_310_420/img/40/27/4027cc2b52f4759c444e28c753ae0b2e.jpg",
+    "Anos após testemunhar a morte do herói Maximus (Russell Crowe) pelas mãos de seu tio (Joaquin Phoenix), Lucius (Paul Mescal) enfrenta um novo desafio: forçado a entrar no Coliseu, ele deve lutar pela sobrevivência e pela honra de Roma. Depois de ter sua casa tomada pelos imperadores tirânicos que governam Roma com punho de ferro, Lucius precisa reencontrar a força e a coragem que o inspiraram na juventude. Admirador da jornada de Maximus, Lucius busca seguir seus passos para restaurar a glória perdida de Roma. Gladiador 2, é uma sequência direta do aclamado filme dirigido por Ridley Scott. Agora, Lucius luta não apenas pela sua vida, mas pelo futuro do império. Com o coração cheio de raiva e o destino de Roma em jogo, ele revisita seu passado em busca da força necessária para devolver ao povo a honra que um dia foi perdida."
+],
+[
+    "Venom: A Última Rodada",
+    "https://th.bing.com/th/id/R.bc403e755bdaa995f19cbace10db1390?rik=fNcSGgAX3sY3vA&pid=ImgRaw&r=0",
+    "https://br.web.img2.acsta.net/c_310_420/img/10/7a/107aa110584ecf393a0e85e1611235f5.jpg",
+    "Venom: A Última Rodada é o terceiro capítulo da franquia. Eddie Brock (Tom Hardy) e Venom estão em uma frenética fuga. Caçados por forças tanto do seu planeta natal quanto da Terra, a dupla se encontra em um aperto crescente, com a rede se fechando rapidamente ao seu redor. À medida que a pressão aumenta, Eddie e Venom são forçados a tomar uma decisão devastadora que marcará o fim de sua última dança juntos. O filme conta com cenas eletrizantes, com Venom devorando inimigos enquanto luta ao lado de Eddie. A situação se complica quando outros alienígenas do planeta de Venom descobrem seu paradeiro e chegam à Terra com a missão de capturá-lo. A aliança inseparável entre Eddie e Venom é posta à prova como nunca antes, enfrentando ameaças implacáveis e perigos de todos os lados. Dirigido por Kelly Marcel, o filme também conta com Juno Temple e Chiwetel Ejiofor, prometendo um desfecho épico e emocionante para a saga."
+],
+[
+    "Pássaro Branco – Uma História de Extraordinário",
+    "https://teoriageek.com.br/wp-content/uploads/2023/04/5_vDloZKI.jpg",
+    "https://br.web.img3.acsta.net/c_310_420/pictures/24/01/17/15/54/2638960.png",
+    "Em Pássaro Branco – Uma História de Extraordinário, logo após os acontecimentos em Extraordinário, o valentão Julian Albans, que fazia bullying com Auggie, deixou a escola Beecher Prep de uma vez por todas. Agora, ainda sem um novo local de estudo, o menino recebe a visita de sua avó Sara Albans (Helen Mirren). De volta de Paris, a senhora conta uma história que vai mudar a vida do garoto. Em plena Segunda Guerra Mundial, a avó conta sua história de quando era menina, da mesma idade que seu neto tem agora. Ela, judia, conta a história de suas dificuldades em uma Alemanha ocupada pelo nazismo e como sobreviveu ao holocausto quando um aluno que não conhecia direito arriscou tudo para dar a ela e seus pais a chance de sobreviver da guerra e dos guardas da SS. A notável história de compaixão e coragem contada por sua avó o transformará."
+],
+[
+    "Robô Selvagem",
+    "https://cinepop.com.br/wp-content/uploads/2024/03/robo-selvagem.jpg",
+    "https://br.web.img3.acsta.net/c_310_420/img/cc/85/cc85122f8c6571afb0102ec2e27daae5.jpg",
+    "Uma nave naufraga numa terra desabitada e dá início à aventura épica do robô Roz, a última unidade das chamadas ROZZUM ainda funcional e inteligente. Preso nesta ilha aparentemente sozinho, Roz precisa sobreviver às intempéries da floresta. Sua única esperança é se adaptar ao ambiente hostil e avesso às suas programações. Para isso, Roz passa a conviver com os animais aprendendo sobre a vida na selva e os modos de sobrevivência na natureza. É durante essa exploração que Roz encontra um filhote de ganso e estabelece como missão cuidá-lo. Desse laço inesperado com o bicho abandonado, Roz se aproxima de uma realidade nova e instigante, construindo uma relação harmoniosa com os animais nativos. Do mesmo diretor de Lilo & Stitch e Como Treinar o Seu Dragão, Robô Selvagem é uma história comovente sobre a convivência entre tecnologia e natureza e o significado de estar vivo."
+],
+[
+    "Megalópolis",
+    "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/08/megalopolis.png",
+    "https://br.web.img3.acsta.net/c_310_420/img/5e/f7/5ef74bf0547f6313536970e42bce2a0c.jpg",
+    "Dirigido por Francis Ford Coppola, Megalópolis conta a história de um arquiteto que quer reconstruir a cidade de Nova York como uma utopia após um desastre devastador. Cesar Catilina (Adam Driver) é esse inventor megalomaníaco e egocêntrico que imagina uma metrópole autossustentável que cresce organicamente com seus habitantes. Para dar prosseguimento a seus planos, porém, Cesar precisa navegar pelos interesses de figuras ricas, ambiciosas e corruptas, além de precisar convencer o prefeito de Nova Roma – nome da cidade em evidência – Franklyn Cicero (Giancarlo Esposito) de suas ideias. Nesse épico ambientado nos tempos modernos, Francis Ford Coppola constrói uma fábula sobre o poder, sonhando com um cenário futurista devastador e frenético."
+],
+[
+    "Meu Malvado Favorito 4",
+    "https://th.bing.com/th/id/OIP.Dlpq7iew3hJNnnPDyoc81AHaEb?rs=1&pid=ImgDetMain",
+    "https://br.web.img3.acsta.net/c_310_420/img/6c/71/6c71afa89fd8ed8999b3e04d8d794a0e.jpg",
+    "Nesta sequência, o vilão mais amado do planeta, que virou agente da Liga Antivilões, retorna para mais uma aventura em Meu Malvado Favorito 4. Agora, Gru (Leandro Hassum), Lucy (Maria Clara Gueiros), Margo (Bruna Laynes), Edith (Ana Elena Bittencourt) e Agnes (Pamella Rodrigues) dão as boas-vindas a um novo membro da família: Gru Jr., que pretende atormentar seu pai. Enquanto se adapta com o pequeno, Gru enfrenta um novo inimigo, Maxime Le Mal (Jorge Lucas) que acaba de fugir da prisão e agora ameaça a segurança de todos, forçando sua namorada mulher-fatal Valentina (Angélica Borges) e a família a fugir do perigo. Em outra cidade, as meninas tentam se adaptar ao novo colégio e Valentina incentiva Gru a tentar viver uma vida mais simples, longe das aventuras perigosas que fez durante quase toda a vida. Neste meio tempo, eles também conhecem Poppy (Lorena Queiroz), uma surpreendente aspirante à vilã e os minions dão o toque que faltava para essa nova fase."
+],
+[
+    "Minha Mãe é uma Peça - O Filme",
+    "https://th.bing.com/th/id/R.57433a3527c19414aa0d8f0e458059ab?rik=X0d2rkq8W1IncA&pid=ImgRaw&r=0",
+    "https://br.web.img3.acsta.net/c_310_420/pictures/210/016/21001687_20130426011958954.jpg",
+    "Dona Hermínia (Paulo Gustavo) é uma mulher de meia idade, divorciada do marido (Herson Capri), que a trocou por uma mais jovem (Ingrid Guimarães). Hiperativa, ela não larga o pé de seus filhos Marcelina (Mariana Xavier) e Juliano (Rodrigo Pandolfo), sem se dar conta que eles já estão bem grandinhos. Um dia, após descobrir que eles consideram ela uma chata, resolve sair de casa sem avisar para ninguém, deixando todos, de alguma forma, preocupados com o que teria acontecido. Mal sabem eles que a mãe foi visitar a querida tia Zélia (Sueli Franco) para desabafar com ela suas tristezas do presente e recordar os bons tempos do passado."
+],
+[
+    "Meninas malvadas",
+    "https://cinepop.com.br/wp-content/uploads/2018/01/meangirls.jpg",
+    "https://br.web.img2.acsta.net/c_310_420/pictures/210/087/21008705_20130527194056821.jpg",
+    "Cady Heron (Lindsay Lohan) é uma garota que cresceu na África e sempre estudou em casa, nunca tendo ido a uma escola. Após retornar aos Estados Unidos com seus pais, ela se prepara para iniciar sua vida de estudante, se matriculando em uma escola pública. Logo Cady percebe como a língua venenosa de suas novas colegas pode prejudicar sua vida e, para piorar ainda mais sua situação, Cady se apaixona pelo garoto errado."
+],
+[   "Valente",
+    "https://mixme.com.br/wp-content/uploads/2023/08/os-melhores-desenhos-da-pixar-na-disney-valente-saiba-mais.jpg",
+    "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/89/92/72/20126088.jpg",
+    "A jovem princesa Merida foi criada pela mãe para ser a sucessora perfeita ao cargo de rainha, seguindo a etiqueta e os costumes do reino. Mas a garota dos cabelos rebeldes não tem a menor vocação para esta vida traçada, preferindo cavalgar pelas planícies selvagens da Escócia e praticar o seu esporte favorito, o tiro ao arco. Quando uma competição é organizada contra a sua vontade, para escolher seu futuro marido, Merida decide recorrer à ajuda de uma bruxa, a quem pede que sua mãe mude. Mas quando o feitiço surte efeito, a transformação da rainha não é exatamente o que Merida imaginava... Agora caberá à jovem ajudar a sua mãe e impedir que o reino entre em guerra com os povos vizinhos."
+],
+[   "As branquelas",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjGgin6SqYjiTSoHF95LZy0fSMj6plmmlHDQ&s",
+    "https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/97/52/82/20534159.jpg",
+    "Em As Branquelas, os irmãos Marcus (Marlon Wayans) e Kevin Copeland (Shawn Wayans) são detetives do FBI que estão com problemas no trabalho. A última investigação da dupla foi um grande fracasso e eles estão sob a ameaça de serem demitidos. Quando um plano para sequestrar as mimadas irmãs Brittany (Maitland Ward) e Tiffany Wilson (Anne Dudek) é descoberto, o caso é entregue aos principais rivais dos irmãos Copeland, os agentes Vincent Gomez (Eddie Velez) e Jack Harper (Lochlyn Munro). Para aumentar ainda mais a humilhação da dupla, eles são escalados para escoltar as jovens mimadas do aeroporto até o local de um evento pelo qual elas esperaram por meses. Porém no trajeto um acidente de carro provoca um verdadeiro desastre: enquanto uma das irmãs arranha o nariz, a outra corta o lábio. Desesperadas, elas se recusam a ir ao evento. É quando,para salvar o emprego, Marcus e Kevin decidem por assumir as identidades das irmãs."
+],
+];
+
+let filmeAtual = 0;
+
+const registrarFilme = (dado) => {
+    let dados = dado;
+    let corpo = document.createElement("div");
+    
+    corpo.style.backgroundImage = `url(${dados[2]})`;
+    document.getElementById("catalogo").appendChild(corpo);
+
+    corpo.addEventListener("click", (e)=>{
+        document.getElementById("principal").style.backgroundImage = `url(${dados[1]})`;
+        document.getElementById("titulo-filme").innerText = dados[0];
+        document.getElementById("desc-filme").innerText = dados[3];
+        window.scrollTo(0, 0);
+        filmeAtual = dados;
+    });
+}
+
+let dado = filmes[Math.floor(Math.random() * filmes.length)];
+filmeAtual = dado;
+document.getElementById("principal").style.backgroundImage = `url(${dado[1]})`;
+document.getElementById("titulo-filme").innerText = dado[0];
+document.getElementById("desc-filme").innerText = dado[3];
+
+for(let x in filmes) {
+    registrarFilme(filmes[x]);
+}
+
+const abrirFilme = () => {
+    localStorage.setItem("filme", JSON.stringify(filmeAtual));
+    window.open("player.html", "popup", 0, 200, 300);
+}
